fb9d0fc5482541fd3352a0b70a35104d71ca9917
ezhevikin <st.nickolas@mail.ru>
2023-08-17 14:37:59 +0500

    Hello

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ subprocess.run(["sudo tar -xf /home/temp/cades.tar.gz -C /home/temp/install_pack
 #subprocess.run(["sh /home/temp/crypto/install.sh lsb-cprocsp-kc1 cprocsp-rdr-gui-gtk cprocsp-cptools-gtk cprocsp-stunnel lsb-cprocsp-pkcs11"], shell=True)
 
 #print("---------------Install_prog_from_pack.list----------------")
-subprocess.run(["sudo xargs -a /home/temp/pack.list apt-get install"], shell=True)
+#subprocess.run(["sudo xargs -a /home/temp/pack.list apt-get install"], shell=True)
 #print("--------------Fix_broken_install-------------")
 #subprocess.run(["sudo apt --fix-broken install"], shell=True)
 
 #subprocess.run(["sudo dpkg -i /home/temp/install_pack/*/*.deb"], shell=True)
+print("Hello")
 print("Hello")
