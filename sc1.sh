938cb685ccec3997471a1810fda3e6a9de6c7d59
ezhevikin <st.nickolas@mail.ru>
2023-08-22 16:54:10 +0500

    +install tasksel  +install lamp +confiq_mysql

--- sc1.sh
+++ sc1.sh
@@ -1,10 +1,7 @@
-sudo apt install apache2
 sudo apt install ufw
 sudo ufw enable
 sudo ufw app list
 sudo ufw allow 'Apache Full'
 sudo ufw allow 'OpenSSH'
-sudo apt install mysql-server
-sudo mysql_secure_installation
 sudo apt install php libapache2-mod-php php-mysql
 # ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
