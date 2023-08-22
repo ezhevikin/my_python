37c3436fd73711cd16d04be9306383cc6106291e
ezhevikin <st.nickolas@mail.ru>
2023-08-22 17:34:17 +0500

    +

--- sc1.sh
+++ sc1.sh
@@ -1,5 +1,5 @@
 sudo apt -y install tasksel
-sudo tasksell install lamp-server
+sudo tasksel install lamp-server
 sudo apt -y install ufw
 sudo ufw enable
 sudo ufw app list
@@ -8,5 +8,4 @@ sudo ufw allow 'OpenSSH'
 sudo apt -y install php libapache2-mod-php php-mysql
 sudo apt -y install phpmyadmin
 sudo mysql
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-exit
+# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
