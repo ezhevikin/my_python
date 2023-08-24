83e77c7eaf82f3c87c90769738382d9861d6ff6c
ezhevikin <st.nickolas@mail.ru>
2023-08-24 16:15:03 +0500

    +sql auth

--- sc1.sh
+++ sc1.sh
@@ -7,8 +7,8 @@ sudo ufw allow 'Apache Full'
 sudo ufw allow 'OpenSSH'
 sudo apt -y install php libapache2-mod-php php-mysql
 sudo apt -y install phpmyadmin
-sudo mysql
-#sudo mysql -u root -p << EOF
+#sudo mysql
+sudo mysql << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '99Enagih/';
 exit
 EOF
