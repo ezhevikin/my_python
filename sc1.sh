86e4c4427bccd8175281d91fea28aa3136873173
ezhevikin <st.nickolas@mail.ru>
2023-08-24 14:08:02 +0500

    111

--- sc1.sh
+++ sc1.sh
@@ -8,4 +8,7 @@ sudo ufw allow 'OpenSSH'
 sudo apt -y install php libapache2-mod-php php-mysql
 sudo apt -y install phpmyadmin
 sudo mysql
-# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+#sudo mysql -u root -p << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '99Enagih/';
+exit
+EOF
